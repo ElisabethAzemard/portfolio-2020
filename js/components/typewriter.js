836b3a655466affce3a14395b7b0a517587e6e3e
Elisabeth Azémard <elie.azemard@gmail.com>
2020-05-14 10:57:16 +0200

    Fix W3C errors & warnings; Remove useless comments in _fonts.scss; Remove contact page: add content in footer. Finish up the timeline content. Fix header quote width on mobile and display author after whole quote is written.

--- js/components/typewriter.js
+++ js/components/typewriter.js
@@ -4,7 +4,7 @@ import Typewriter from 'typewriter-effect/dist/core';
 document.addEventListener('DOMContentLoaded', () => {
 
     const quote = new Typewriter('#typewriter', {
-        loop: true,
+        loop: false,
         deleteSpeed: 25
     });
 
@@ -15,7 +15,22 @@ document.addEventListener('DOMContentLoaded', () => {
         .pauseFor(1000)
         .deleteChars(9)
         .typeString('writing."')
-        .pauseFor(10000)
         .start()
+
+    const quoteMobile = new Typewriter('#typewriterMobile', {
+        loop: false,
+        deleteSpeed: 25
+    });
+
+    quoteMobile.typeString('"You can make<br>anything<br>by <span class="has-text-primary">coding</span>')
+        .pauseFor(1000)
+        .deleteChars(6)
+        .typeString('<span class="has-text-warning">designing</span>')
+        .pauseFor(1000)
+        .deleteChars(9)
+        .typeString('writing."')
+        .start()
+
+    setTimeout(() => { document.querySelector('#quoteAuthor').classList.add('is-opaque') }, 12000);
 
 });
